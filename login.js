6b1a3f4cb06a0c00f8dcb7ec07d0c05bc2279f60
griprodata <70912251+griprodata@users.noreply.github.com>
2020-11-03 23:26:38 +0300

    деккаративные элемены

--- login.js
+++ login.js
@@ -1,2 +1,14 @@
+const loginLink = document.querySelector(".footer-contact-link");
+const loginPopup =document.querySelector(".modal-email");
+const loginClose =document.querySelector(".modal-close")
+const loginLogin =document.querySelector(".login-icon")
+loginLink.addEventListener("click",function (evt) {
+evt.preventDefault();
+loginPopup.classList.add("modal-show");
+loginLogin.focus();
+});
 
-
+loginClose.addEventListener("click", function (evt) {
+evt.preventDefault();
+loginPopup.classList.remove("modal-show");
+});
